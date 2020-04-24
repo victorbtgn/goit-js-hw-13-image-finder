2e72bef28a5f7a80e7e40a38287aa8d079b8e04e
Victor Batigin <bixs85@gmail.com>
2020-04-24 20:43:35 +0300

    try infinite-scroll

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import debounce from 'lodash.debounce';
 import PNotify from './js/pnotify';
 import * as basicLightbox from 'basiclightbox';
 import '../node_modules/basiclightbox/src/styles/main.scss';
+import InfiniteScroll from 'infinite-scroll';
 
 import refs from './js/refs';
 import fetchImageQuery from './js/apiService';
@@ -10,21 +11,27 @@ import markup from './js/gallery-markup';
 import './styles.css';
 
 refs.form.addEventListener('input', debounce(onInput, 500));
-refs.gallery.addEventListener('click', onImageClick);
+refs.gallery.addEventListener('click', onOpenModal);
+
+// let page = 1;
+// let query = '';
 
 function onInput(event) {
   event.preventDefault();
   const query = event.target.value;
+  // page = 1;
 
   if (query.length === 0) {
     refs.gallery.innerHTML = '';
     return;
   }
 
-  fetchImageQuery(query).then(data => markup(data));
+  fetchImageQuery(query)
+    .then(data => markup(data))
+    .then(infScroll);
 }
 
-function onImageClick(event) {
+function onOpenModal(event) {
   event.preventDefault();
 
   if (event.target.nodeName !== 'IMG') {
@@ -39,3 +46,20 @@ function onImageClick(event) {
 
   instance.show();
 }
+
+const apiKey = '16159179-9a5d2f4d64cb4ee75e82dc2d4';
+
+const infScroll = new InfiniteScroll('.gallery', {
+  pageCount: 1,
+  path: function () {
+    return (
+      'https://pixabay.com/api/?image_type=photo&orientation=horizontal&page=' +
+      this.pageIndex +
+      '&per_page=5&key=' +
+      apiKey
+    );
+  },
+  append: '.photo-card',
+  history: false,
+  // status: '.page-load-status',
+});
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,10 +2,12 @@ const apiKey = '16159179-9a5d2f4d64cb4ee75e82dc2d4';
 
 function fetchImageQuery(query) {
   return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&key=${apiKey}`,
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=1&per_page=5&key=${apiKey}`,
   )
     .then(res => res.json())
     .then(data => data.hits);
 }
 
 export default fetchImageQuery;
+
+// &page=${page}
